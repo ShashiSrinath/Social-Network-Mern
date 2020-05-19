ff00afe78b924d4ed0cf25a4e1b41bbc6509cc54
Shashi Srinath <atshashisrinth@gmail.com>
2020-05-19 18:59:34 +0530

    added ssl redirection

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const sslRedirect = require('heroku-ssl-redirect');
 const PORT = process.env.PORT || 4000;
 const DB_URI = process.env.DB_URI || 'mongodb://localhost:27017/dbname';
 
@@ -22,6 +23,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+app.use(sslRedirect());
 
 
 // ----------------------------------------Routes----------------------------------------------- //
